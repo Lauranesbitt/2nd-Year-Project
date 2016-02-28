46b636d82b359ffda4f2dece9484ee6b800037d5
kevm66 <kevm66@gmail.com>
2016-02-28 16:50:23 +0000

    created project

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include 'database.php'; ?>
+<?php include 'header.php'?>
+<!-- Body START -->
+This is index.php
+<div class="page-content">
+    <div class="mdl-grid">
+        <div class="mdl-cell mdl-cell--12">
+            <!-- left column -->
+            <div class="mdl-cell mdl-cell-8">
+                left hello
+            </div>
+            <!-- right column -->
+            <div class="mdl-cell mdl-cell-4">
+                right
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ./Body END -->
+<?php include 'footer.php'?>
